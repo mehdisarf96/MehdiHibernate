c08ca75bafbc899e529c107646f7f32e82f0696a
M.Mehdi <m.sfj9696@gmail.com>
2022-04-12 06:07:33 +0430

    Create a table in DB just with creating a domain model class (annotated with @Entity) and an EntityManagerFactory.

--- src/main/java/com/mehdisarf/driver/Main.java
+++ src/main/java/com/mehdisarf/driver/Main.java
@@ -1,7 +1,13 @@
 package com.mehdisarf.driver;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Just this line. Run and see what happens.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mehdiPersistenceUnit");
     }
 }
