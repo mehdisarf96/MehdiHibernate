4173763d4d6ee001aac6a442132d82a099f14d2a
M.Mehdi <m.sfj9696@gmail.com>
2022-04-12 06:34:18 +0430

    Create a Person object and persist that in DB using EntityManager.

--- src/main/java/com/mehdisarf/driver/Main.java
+++ src/main/java/com/mehdisarf/driver/Main.java
@@ -1,13 +1,27 @@
 package com.mehdisarf.driver;
 
+import com.mehdisarf.domainmodel.Person;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Just this line. Run and see what happens.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("mehdiPersistenceUnit");
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin(); // Begin Transaction
+
+        Person person = new Person(452L, "Mehdi", "Sarf");
+        entityManager.persist(person);
+
+        transaction.commit(); // Commit Transaction
+
+        entityManager.close();
     }
 }
